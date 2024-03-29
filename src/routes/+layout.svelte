d4ffbee88d62424ac5b761afa8a5a226dbd4b321
robertoman37 <92748186+robertoman37@users.noreply.github.com>
2024-03-29 12:58:38 +0000

    start of navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<style>
+    nav {
+        border-bottom: 1px solid black;
+    }
+    h1 a {
+        text-decoration: none;
+    }
+</style>
+<nav>
+    <h1><a href="/">Wecan2</a></h1>
+</nav>
+
+<slot />
