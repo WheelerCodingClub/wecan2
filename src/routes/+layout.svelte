d5446f88f94f30c98980072c4fb98cdb1d13a105
robertoman37 <robertoman7th@gmail.com>
2024-04-03 15:25:02 -0400

    start of css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,44 @@
 <style>
+    nav {
+        padding: .5rem 1rem;
+        display: flex;
+        align-items: center;
+        flex-wrap: nowrap;
+        flex-direction: row;
+        box-shadow: 1px 1px 5px rgb(148,157,166);
+    }
+    .container {
+        width: 100%;
+        max-width: 1140px;
+        margin: 0 auto;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+    a {
+        text-decoration: none;
+    }
+    .title {
+        text-align: left;
+        font-size: 30px;
+    }
+    .links-left {
+        text-align: left;
+    }
+    .links-right {
+        text-align: right;
+    }
+    ul {
+        width: fit-content;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+    li {
+        margin: auto 0;
+        display: inline;
+        list-style-type: none;
+    }
 </style>
 <nav>
     <div class="container">
