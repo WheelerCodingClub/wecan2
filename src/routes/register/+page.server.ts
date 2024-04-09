a5a8fdb75e0d625e0ce9a49b608fe3b0e00fddc3
wackbyte <wackbyte@pm.me>
2024-04-09 12:45:23 -0400

    more informative form validation

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,4 +1,5 @@
 import type { Actions, PageServerLoad } from "./$types";
+import type { Error } from "./error";
 import { fail, redirect } from "@sveltejs/kit";
 
 export const load: PageServerLoad = ({ locals: { user } }) => {
@@ -14,8 +15,24 @@ export const actions: Actions = {
         const email = data.get("email");
         const password = data.get("password");
 
-        // missing fields
-        if (!name || !email || !password) return fail(400, { name, email, missing: true });
+        const sfail = (status: number, error: Error) => fail(status, { name, email, error });
+
+        if (!name) return sfail(400, "name_missing");
+        if (typeof name !== "string") return sfail(400, "name_invalid");
+        if (name.length > 64) return sfail(400, "name_long"); // TODO: determine suitable max length
+
+        if (!email) return sfail(400, "email_missing");
+        if (typeof email !== "string") return sfail(400, "email_invalid"); // TODO: check if email is valid
+        if (email.length > 128) return sfail(400, "email_long"); // TODO: determine suitable max length
+        // TODO: check if email is already registered
+        // return sfail(400, "email_taken");
+
+        if (!password) return sfail(400, "password_missing");
+        if (typeof password !== "string") return sfail(400, "password_invalid");
+        if (password.length < 8) return sfail(400, "password_short");
+        if (password.length > 72) return sfail(400, "password_long");
+
+        // TODO: repeat password
 
         // TODO: registration
 
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
     import type { ActionData } from "./$types";
+    import type { Error } from "./error";
     import { enhance } from "$app/forms";
 
     export let form: ActionData;
+
+    const errorMessages: Record<Error, string> = {
+        name_missing: "Name is required.",
+        name_invalid: "Invalid name.",
+        name_long: "That name is too long.",
+        email_missing: "Email address is required.",
+        email_invalid: "Invalid email address.",
+        email_long: "That email address is too long.",
+        email_taken: "That email address is already registered.",
+        password_missing: "Password is required.",
+        password_invalid: "Invalid password.",
+        password_short: "That password is too short.",
+        password_long: "That password is too long.",
+    };
 </script>
 
 <form method="POST" use:enhance>
-    {#if form?.missing}
-        <p>Missing required fields.</p>
+    {#if form?.error}
+        <p>{errorMessages[form.error]}</p>
     {/if}
     <label>
         Name
