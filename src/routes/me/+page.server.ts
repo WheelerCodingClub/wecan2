f0621c9abbbd40ce4711da159cd82430f0a4b665
wackbyte <wackbyte@pm.me>
2024-04-10 14:00:26 -0400

    simple account page

---
+++ src/routes/me/+page.server.ts
@@ -0,0 +1,18 @@
+import type { PageServerLoad } from "./$types";
+import { error } from "@sveltejs/kit";
+import sql from "$lib/server/db";
+
+export const load: PageServerLoad = async ({ locals: { user } }) => {
+    if (!user) error(403, "Forbidden");
+
+    const [{ name, email, created }] = await sql`
+        SELECT name, email, created FROM users WHERE id = ${user.id}
+    `;
+
+    return {
+        id: user.id,
+        name,
+        email,
+        created,
+    };
+};
---
+++ src/routes/me/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import type { PageData } from "./$types";
+
+    export let data: PageData;
+</script>
+
+<h1>My Awesome Account Settings Page</h1>
+<dl>
+    <dt>ID</dt>
+    <dd><code>{data.id}</code></dd>
+    <dt>Name</dt>
+    <dd><code>{data.name}</code></dd>
+    <dt>Email</dt>
+    <dd><code>{data.email}</code></dd>
+    <dt>Created at</dt>
+    <dd><code>{data.created}</code></dd>
+</dl>
