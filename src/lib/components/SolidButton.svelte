2fc899b8c9b4292018ac4d2bd5800e1c57b5fcde
robertoman37 <92748186+robertoman37@users.noreply.github.com>
2024-04-11 14:32:54 +0000

    Replace buttons with components

---
+++ src/lib/components/SolidButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let href: string;
+</script>
+<a {href}><slot>button</slot></a>
+<style>
+    a {
+        display: block;
+        background: #4E9CAF;
+        padding: 8px;
+        text-align: center;
+        border-radius: 5px;
+        color: white;
+    }
+</style>
