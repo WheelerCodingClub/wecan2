1367e078b5f525ba6703d267a277fef7e0d4deb1
robertoman37 <92748186+robertoman37@users.noreply.github.com>
2024-04-11 02:03:14 +0000

    More css work (I give up)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,22 +6,23 @@
 
 <nav>
     <div class="container">
-        <a href="/">
-            <span class="title">Wecan2</span>
-        </a>
         <ul class="links-left">
-            <li><a href="/">Home</a></li>
+            <li class="title"><a href="/">Wecan2</a></li>
+            <!--The .title right next to it has the same
+            function, so I removed it.-->
             <li><a href="/#">About</a></li>
-            <li><a href="/#">Contact Us</a></li>
+            <!--Can't we just put contact in about?-->
         </ul>
         <ul class="links-right">
             <li><a href="/#">Join a Club</a></li>
-            <li><a href="/#">Register School</a></li>
+            <!-- Since this is really just a wheeler thing,
+            I don't think we need a register school option.
+            If we need to we'll add it back later.-->
             {#if data.user}
                 <li><a href="/logout">Log Out</a></li>
             {:else}
-                <li><a href="/login">Log In</a></li>
-                <li><a href="/register">Register</a></li>
+                <li><a href="/login" class="unfilled_button">Log In</a></li>
+                <li><a href="/register" class="filled_button">Register</a></li>
             {/if}
         </ul>
     </div>
@@ -32,6 +33,12 @@
 </main>
 
 <style>
+    nav, div, a, ul, li {
+        padding: 0;
+        margin: 0;
+
+    }
+
     nav {
         padding: .5rem 1rem;
         display: flex;
@@ -40,6 +47,7 @@
         flex-direction: row;
         box-shadow: 1px 1px 5px rgb(148,157,166);
     }
+
     .container {
         width: 100%;
         max-width: 1140px;
@@ -48,28 +56,53 @@
         flex-direction: row;
         justify-content: space-between;
     }
-    a {
-        text-decoration: none;
-    }
+
     .title {
-        text-align: left;
         font-size: 30px;
-    }
-    .links-left {
-        text-align: left;
-    }
-    .links-right {
-        text-align: right;
-    }
-    ul {
-        width: fit-content;
-        height: 100%;
-        margin: 0;
-        padding: 0;
-    }
-    li {
-        margin: auto 0;
         display: inline;
-        list-style-type: none;
+        margin: 0 2%;
+    }
+
+    .links-left {
+        float: left;
+    }
+
+    .links-right {
+        float: right;
+    }
+
+    ul {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        list-style: none;
+    }
+    
+    .filled_button {
+        display: block;
+        background: #4E9CAF;
+        padding: 8px;
+        text-align: center;
+        border-radius: 5px;
+        color: white;
+    }
+    .unfilled_button {
+        display: block;
+        border: 3px solid #4E9CAF;
+        width: 60px;
+        padding: 5px;
+        text-align: center;
+        border-radius: 5px;
+    }
+
+    li {
+        margin: 0 2%;
+        color: black;
+    }
+
+    li a {
+        text-decoration: none;
+        color: black;
+        font-weight: bold;
     }
 </style>
