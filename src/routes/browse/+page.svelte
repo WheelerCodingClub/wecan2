541938a63f31e1d1dc5f247dc19e0b644b411e24
robertoman37 <robertoman7th@gmail.com>
2024-04-14 17:15:06 -0400

    themed browse page

--- src/routes/browse/+page.svelte
+++ src/routes/browse/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import type { PageData } from "./$types";
 
+    import SolidButton from "$lib/components/SolidButton.svelte";
+
     export let data: PageData;
 </script>
 
@@ -8,8 +10,41 @@
 <ul>
     {#each data.clubs as club (club.id)}
         <li>
-            <h2><a href="/view/{club.id}">{club.name}</a> <code>[ID: {club.id}]</code></h2>
-            <p>{club.description}</p>
+            <h2>{club.name}</h2>
+            <code>[ID: {club.id}]</code>
+            <h4>Description:</h4>
+            <p id="description">{club.description}</p>
+            <SolidButton
+                href="/view/{club.id}"
+                --button-margin="0 auto"
+                --button-width="50%">
+                View More
+            </SolidButton>
         </li>
     {/each}
 </ul>
+
+<style>
+    ul {
+        list-style: none;
+    }
+
+    li {
+        border-radius: 10px;
+        box-shadow: 1px 1px 5px rgb(148,157,166);
+        padding: 1%;
+        margin: 2% auto;
+        width: 30%;
+    }
+
+    h2 {
+        text-align: center;
+    }
+
+    #description {
+        border: 1px solid rgb(148,157,166);
+        border-radius: 10px;
+        padding: 15px;
+    }
+    
+</style>
