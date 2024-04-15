65221221708ba3854a97e6aa4c004c87c0ff629b
17680138 <17680138@mailbox.org>
2024-04-15 11:57:12 -0400

    implement dropdown with CSS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,6 @@
     import HollowButton from "$lib/components/HollowButton.svelte";
 
     export let data: LayoutData;
-
-    let hovered: boolean;
 </script>
 
 <nav>
@@ -18,23 +16,14 @@
             {#if data.loggedIn}
                 <li class="buttonList"><HollowButton href="/browse">Join a Club</HollowButton></li>
                 <li class="buttonList">
-                    <SolidButton 
-                    on:mouseover={() => hovered = true}
-                    on:mouseout={() => hovered = false}>
-                    Account
-                    </SolidButton>
+                    <div class="dropdown">
+                        <SolidButton>Account</SolidButton>
+                        <div class="dropdown-content">
+                            <a href="/me">Account Information</a>
+                            <a href="/logout">Logout</a>
+                        </div>
+                    </div>
                 </li>
-                <!-- svelte-ignore a11y-no-static-element-interactions -->
-                <div
-                class="dropdown-options"
-                class:hovered
-                on:focus
-                on:blur
-                on:mouseover={() => hovered = true}
-                on:mouseout={() => hovered=false}>
-                    <a href="/me">Account Information</a>
-                    <a href="/logout">Logout</a>
-                </div>
             {:else}
                 <li><a href="/register">Join a Club</a></li>
                 <li class="buttonList"><HollowButton href="/login">Log In</HollowButton></li>
@@ -104,7 +93,7 @@
         font-weight: bold;
     }
 
-    .dropdown-options {
+    .dropdown-content {
         display: none;
         position: absolute;
         top: 60px;
@@ -114,16 +103,16 @@
         border-radius: 10px;
     }
 
-    .hovered {
-        display: block;
-    }
-
-    .dropdown-options a {
+    .dropdown-content a {
         display: block;
         color: black;
         padding: 2px;
         text-decoration: none;
         padding: 20px 40px;
+    }
+
+    .dropdown:hover .dropdown-content {
+        display: block;
     }
 
     * {
