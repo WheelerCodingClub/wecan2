5153de1027cad5445fddad17fe2efa7a1050bdb7
wackbyte <wackbyte@pm.me>
2024-05-02 18:47:21 -0400

    hack together basic post creation and listing

--- src/routes/view/[id]/+page.svelte
+++ src/routes/view/[id]/+page.svelte
@@ -24,3 +24,18 @@
     <dt>Created at</dt>
     <dd><code>{data.club.created}</code></dd>
 </dl>
+<h2>Posts</h2>
+<ul>
+    {#each data.posts as post (post.id)}
+        <li>
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+            <dl>
+                <dt>ID</dt>
+                <dd><code>{post.id}</code></dd>
+                <dt>Created at</dt>
+                <dd><code>{post.created}</code></dd>
+            </dl>
+        </li>
+    {/each}
+</ul>
