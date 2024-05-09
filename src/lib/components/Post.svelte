338950aa544224d4d6ed709007f297a43878d2ae
robertoman37 <robertoman7th@gmail.com>
2024-05-08 21:02:11 -0400

    Post.svelte added

---
+++ src/lib/components/Post.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let title: string;
+    export let description: string;
+    export let images: {src: string, alt: string}[] = [ ];
+    export let club: string;
+
+    import '$lib/styles/forms.css';
+
+    import Gallery from '$lib/components/Gallery.svelte';
+</script>
+
+<div class="card">
+    <h2>{title}</h2>
+    <p>{description}</p>
+    <Gallery {images} />
+    <p>Club: {club}</p>
+</div>
