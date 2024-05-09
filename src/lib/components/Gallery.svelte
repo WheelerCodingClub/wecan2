34d6dc1e5b28eb00f4157e36e5a5b2c2dc62fea9
robertoman37 <robertoman7th@gmail.com>
2024-05-08 20:57:20 -0400

    Added Gallery.svelte (UNTESTED and UNSTYLED)

---
+++ src/lib/components/Gallery.svelte
@@ -0,0 +1,20 @@
+/*
+  A gallery component to mainly be used within posts. Will likely be removed
+  if used exclusively within Post.svelte
+*/
+<script lang="ts">
+    export let images: {src: string, alt: string}[];
+    let counter: number = 0;
+</script>
+
+<div>
+    {#if images}
+        {#if images.length === 1}
+            <img src={images[1].src} alt={images[1].alt}>
+        {:else}
+            <img src={images[counter].src} alt={images[counter].alt}>
+            <button on:click={() => counter++}>Next image</button>
+            <button on:click={() => counter--}>Previous image</button>
+        {/if}
+    {/if}
+</div>
