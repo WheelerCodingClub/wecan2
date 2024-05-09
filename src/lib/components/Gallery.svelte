87f1b45c50d109f74787177830f0bd3ae9d6d911
Robertoman <92748186+robertoman37@users.noreply.github.com>
2024-05-09 20:03:22 +0000

    buttons get disabled

--- src/lib/components/Gallery.svelte
+++ src/lib/components/Gallery.svelte
@@ -1,7 +1,7 @@
-/*
+<!--
   A gallery component to mainly be used within posts. Will likely be removed
   if used exclusively within Post.svelte
-*/
+-->
 <script lang="ts">
     export let images: {src: string, alt: string}[];
     let counter: number = 0;
@@ -13,8 +13,10 @@
             <img src={images[1].src} alt={images[1].alt}>
         {:else}
             <img src={images[counter].src} alt={images[counter].alt}>
-            <button on:click={() => counter++}>Next image</button>
-            <button on:click={() => counter--}>Previous image</button>
+            <button on:click={() => counter++}
+                disabled={counter === images.length - 1}>Next image</button>
+            <button on:click={() => counter--}
+                disabled={counter === 0}>Previous image</button>
         {/if}
     {/if}
 </div>
