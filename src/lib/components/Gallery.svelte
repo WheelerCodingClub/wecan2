a40143bda293db0ef727e1f9c8667bc71bf6cd4b
Robertoman <92748186+robertoman37@users.noreply.github.com>
2024-05-19 14:01:29 +0000

    temporary commit (LIKELY DOES NOT WORK)

--- src/lib/components/Gallery.svelte
+++ src/lib/components/Gallery.svelte
@@ -7,16 +7,38 @@
     let counter: number = 0;
 </script>
 
-<div>
+<div class="img-container">
     {#if images}
         {#if images.length === 1}
-            <img src={images[1].src} alt={images[1].alt}>
+            <img src={images[0].src} alt={images[0].alt}>
         {:else}
             <img src={images[counter].src} alt={images[counter].alt}>
-            <button on:click={() => counter++}
+            <button class="right"
+                on:click={() => counter++}
                 disabled={counter === images.length - 1}>Next image</button>
-            <button on:click={() => counter--}
+            <button class="left"
+                on:click={() => counter--}
                 disabled={counter === 0}>Previous image</button>
         {/if}
     {/if}
 </div>
+
+<style>
+    div {
+        overflow: hidden;
+        position: relative;
+        z-index: 5;
+    }
+
+    .right {
+        position: absolute;
+        top: 50%;
+        right: 0;
+    }
+
+    .left {
+        position: absolute;
+        top: 50%;
+        left: 0;
+    }
+</style>
