dab1c0f27cf979d9f21a76741580c83b20894fea
Robertoman <92748186+robertoman37@users.noreply.github.com>
2024-05-21 15:28:16 +0000

    temp commit

--- src/lib/components/Gallery.svelte
+++ src/lib/components/Gallery.svelte
@@ -3,22 +3,25 @@
   if used exclusively within Post.svelte
 -->
 <script lang="ts">
+    import HollowButton from './HollowButton.svelte';
+    import '$lib/styles/forms.css'
+
     export let images: {src: string, alt: string}[];
     let counter: number = 0;
 </script>
 
-<div class="img-container">
+<div class="card">
     {#if images}
         {#if images.length === 1}
             <img src={images[0].src} alt={images[0].alt}>
         {:else}
             <img src={images[counter].src} alt={images[counter].alt}>
-            <button class="right"
-                on:click={() => counter++}
-                disabled={counter === images.length - 1}>Next image</button>
-            <button class="left"
+            <HollowButton class="left"
                 on:click={() => counter--}
-                disabled={counter === 0}>Previous image</button>
+                disabled={counter === 0}>&lt;</HollowButton>
+            <HollowButton class="right"
+                on:click={() => counter++}
+                disabled={counter === images.length - 1}>&gt;</HollowButton>
         {/if}
     {/if}
 </div>
@@ -28,7 +31,8 @@
         overflow: hidden;
         position: relative;
         display: flex;
-        width: 50%;
+        width: 250px;
+        height: 250px;
         margin: 0 auto;
         justify-content: space-around;
         align-items: center;
--- src/lib/components/SolidButton.svelte
+++ src/lib/components/SolidButton.svelte
@@ -1,7 +1,14 @@
 {#if $$props.href}
-    <a class={$$props.class || ""} href={$$props.href}><slot>button</slot></a>
+    <a class={$$props.class || ""}
+        href={$$props.href}><slot>button</slot></a>
 {:else}
-    <button class={$$props.class || ""} disabled={$$props.disabled || ""} on:click on:focus on:mouseover on:blur on:mouseout>
+    <button class={$$props.class || ""}
+        disabled={$$props.disabled || ""}
+        on:click 
+        on:focus 
+        on:mouseover 
+        on:blur 
+        on:mouseout>
         <slot>button</slot>
     </button>
 {/if}
