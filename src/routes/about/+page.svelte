d229e21b0352a954362fb0bd31d5c9d5706352fd
blt-sandwich-8704 <88751815+NickTheEASDude@users.noreply.github.com>
2024-11-04 20:06:10 +0000

    update /about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,8 +7,10 @@
 <svelte:head>
     <title>WECAN 2</title>
 </svelte:head>
-<p>Thank you Nicolas</p>
-<h1>Really long scrolling to test navbar</h1>
+<h1>About Us</h1>
+<p>Nothing here yet, don't know what's gonna be here tbh.</p>
+<hr/>
+<h3>Really long scrolling to test navbar</h3>
 {#each arr as item}
     <p>{item}</p>
 {/each}
