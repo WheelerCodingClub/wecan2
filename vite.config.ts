9c43b6d5c061dc4b3b192057399d25034553a96e
blt-sandwich-8704 <88751815+NickTheEASDude@users.noreply.github.com>
2025-01-07 20:22:22 +0000

    Add SSL certificate and key so HTTPS on dev works

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,25 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import { readFile } from "node:fs/promises";
+
+
+let cert: Buffer | undefined;
+let key: Buffer | undefined;
+try {
+	await Promise.all([
+		readFile("cert.pem").then((result) => cert = result),
+		readFile("key.pem").then((result) => key = result),
+	])
+} catch {}
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: cert || key
+	? {
+		https: {
+			cert,
+			key,
+		},
+		proxy: {}
+	} : undefined,
 });
