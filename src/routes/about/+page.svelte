0547bd1aaf10c1fd036aa4c15ef584eb91d5f0c5
blt-sandwich-8704 <88751815+NickTheEASDude@users.noreply.github.com>
2025-02-27 21:01:15 +0000

    Update some stuff.

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
+    import { page } from "$app/state";
     const arr: number[] = [];
     for (let i  = 0; i < 100; i++) {
         arr.push(i);
     }
+    const joinRedirect: boolean = page.url.searchParams.has("joinRedirect");
 </script>
 
 <svelte:head>
     <title>About Us</title>
 </svelte:head>
 
+{#if joinRedirect}
+    <p style="text-align: center; font-weight: bold; margin: 0px;">Redirected from Home</p>
+{/if}
 <h1>About Us</h1>
 <br/>
 <p>Nothing here yet, don't know what's gonna be here tbh.</p>
